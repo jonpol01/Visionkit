a88570169279cccaa2c7b0726386c2e0e461ebe0
John Paul Soliva <soliva.johnpaul@icloud.com>
2026-02-24 19:36:19 +0900

    Initial Commit

---
+++ VisionkitTests/VisionkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionkitTests.swift
+//  VisionkitTests
+//
+//  Created by JOHN PAUL SOLIVA on 2026/02/24.
+//
+
+import Testing
+@testable import Visionkit
+
+struct VisionkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
